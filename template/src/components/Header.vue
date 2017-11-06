7b1d671bd33dc94bc7b850aa0892bd26b414be06
Lemon <lemon@meishakeji.com>
2017-11-06 14:45:29 +0800

    初始化，公共组件添加

--- template/src/components/Header.vue
+++ template/src/components/Header.vue
@@ -1,5 +1,20 @@
 <template>
-
+  <section class="view-header">
+      <a class="logo-left">
+        <span>梅沙教育</span>
+      </a>
+      <ul class="right-link">
+        <li>
+          <a>账户权限</a>
+        </li>
+        <li>
+          <a>个人设置</a>
+        </li>
+        <li>
+          <a>退出</a>
+        </li>
+      </ul>
+  </section>
 </template>
 
 <script>
@@ -8,7 +23,7 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
-
+<style lang="scss" rel="stylesheet/scss"  scoped>
+  @import './header.scss'
 </style>
 
--- template/src/views/home.vue
+++ template/src/views/home.vue
@@ -1,13 +1,55 @@
 <template>
+  <section class="view-all">
+    <vHeader></vHeader>
+    <vMenu v-on:changeMenu="changeMenu"></vMenu>
+    <section class="view-right" :class="{'is-change':isShow}">
 
+    </section>
+  </section>
 </template>
 
 <script>
+import vHeader from '../components/Header.vue';
+import vMenu from '../components/Menu.vue';
 export default {
-  name: 'Home'
+  name: 'Home',
+  data () {
+    return {
+      isShow: false
+    };
+  },
+  methods: {
+    changeMenu (status) {
+      this.isShow = status;
+    }
+  },
+  components: {
+    vHeader,
+    vMenu
+  }
 };
 </script>
 
-<style>
-
+<style lang="scss" rel="stylesheet/scss" scoped>
+  @import "../assets/scss/index";
+  .view-all{
+    overflow: hidden;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    min-width: 1103px;
+    .view-right{
+      position: absolute;
+      top: 50px;
+      left: 180px;
+      right: 0;
+      bottom: 0;
+      @include transition(left .3s ease);
+    }
+    .view-right.is-change{
+      left: 64px;
+    }
+  }
 </style>
