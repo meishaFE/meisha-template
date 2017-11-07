ab7cff78a4a748db942a4691b40659a89811676b
Lemon <lemon@meishakeji.com>
2017-11-07 09:51:35 +0800

    初始化提交

--- template/src/router/index.js
+++ template/src/router/index.js
@@ -4,18 +4,36 @@ import Router from 'vue-router';
 Vue.use(Router);
 
 export default new Router({
-  mode: 'history',
-  routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: () => import('@/views/Home'),
-      children: []
-    },
-    {
-      path: '/404',
-      name: '404',
-      component: () => import('@/views/404')
-    }
-  ]
+    mode: 'history',
+    routes: [
+      {
+          path: '/',
+          name: 'Home',
+          component: () =>import ('@/views/Home'),
+          children: [
+            {
+              path: '/welcome',
+              name: 'Welcome',
+              component: () =>import ('@/views/welcome')
+            },
+            {
+              path: '/order',
+              name: 'Order',
+              component: () =>import ('@/views/module/order/orderHome'),
+              children: [
+                {
+                  path: '/orderList',
+                  name: 'OrderList',
+                  component: () =>import ('@/views/module/order/orderList')
+                }
+              ]
+            }
+          ]
+      },
+      {
+        path: '/*',
+        name: '404',
+        component: () =>import ('@/views/404')
+      }
+    ]
 });
