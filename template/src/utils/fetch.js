55aa7b7583ded9ecd5560636cf0c312aeead6530
jayden <jxdjayden@gmail.com>
2017-11-08 14:06:08 +0800

    fix(browser): fix fetch bug

--- template/src/utils/fetch.js
+++ template/src/utils/fetch.js
@@ -3,6 +3,7 @@
  * 已封装 Auth 到 Header 里面，默认会处理错误的响应，返回 Promise
  */
 import axios from 'axios';
+import URLSearchParams from 'url-search-params';
 import {BASE_URL} from '@/config/api';
 import {isType} from './type';
 
@@ -11,6 +12,13 @@ const options = {
   headers: {
     'Content-Type': 'application/x-www-form-urlencoded'
   },
+  transformRequest: [
+    function (data) {
+        const params = new URLSearchParams();
+        params.append('data', JSON.stringify(data));
+        return params;
+    }
+  ],
   withCredentials: true
 };
 
@@ -26,7 +34,7 @@ $http
   .response
   .use(res => {
     // 处理响应为空的情况
-    if (!res || !res.data || !isType(res.data, 'Object') || !isType(res.data, 'Array')) { // 处理未登录的情况
+    if (!res || !res.data || (!isType(res.data, 'Object') && !isType(res.data, 'Array'))) { // 处理未登录的情况
       return Promise.reject(new Error('response is empty'));
     } else if (res.data.code === -1001) { // 处理后端返回的 code 不为 0 的情况
       handleNoLogin();
